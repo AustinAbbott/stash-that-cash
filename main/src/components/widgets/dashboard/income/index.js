7a092f30920872d74d24b3b4b255a111445065cc
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2021-11-14 20:01:12 -0700

    Create Income Widget

---
+++ main/src/components/widgets/dashboard/income/index.js
@@ -0,0 +1,22 @@
+import { h, Component } from "preact";
+import style from "./style.css";
+
+class Income extends Component {
+
+    render() {
+        return (
+            <div>hi</div>
+        )
+    }
+}
+
+// sources of income - 
+
+// salary
+// pensions
+// bonuses
+// 401k contributions
+// HSA contributions
+
+export default Income;
+
