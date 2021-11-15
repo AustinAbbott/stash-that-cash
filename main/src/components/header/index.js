b15ce7512f7a86def48dbba007019f422a6ecc3f
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2021-11-14 20:03:34 -0700

    Remove Extra Routes From Header

--- main/src/components/header/index.js
+++ main/src/components/header/index.js
@@ -4,13 +4,11 @@ import style from './style.css';
 
 const Header = () => (
 	<header class={style.header}>
-		<h1>Preact App</h1>
+		<h1>Stash That Cash</h1>
 		<nav>
-			<Link activeClassName={style.active} href="/">All</Link>
-			<Link activeClassName={style.active} href="/four-percent">Four Percent Rule</Link>
-			<Link activeClassName={style.active} href="/car">Car</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
+			<Link activeClassName={style.active} href="/">Dashboard</Link>
+			<Link activeClassName={style.active} href="/investing">Investing</Link>
+			<Link activeClassName={style.active} href="/cars">Cars</Link>
 		</nav>
 	</header>
 );
