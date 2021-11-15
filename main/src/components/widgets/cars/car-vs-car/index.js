ffbdfb06971afdccc43e7a5064a22dcd003f9d0e
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2021-11-14 20:04:15 -0700

    Create CarVsCar Component

--- main/src/components/widgets/cars/car-vs-car/index.js
+++ main/src/components/widgets/cars/car-vs-car/index.js
@@ -24,135 +24,186 @@ class CarVsCar extends Component {
   handleSubmit(event) {
     console.log("handleSubmit");
     event.preventDefault();
+
+    let carTotal1 =
+      this.state.purchasePrice1 * this.state.salesTax1 +
+      this.state.estimatedInsurance1 +
+      this.state.annualMaintenance1;
+
+    let carTotal2 =
+      this.state.purchasePrice2 * this.state.salesTax2 +
+      this.state.estimatedInsurance2 +
+      this.state.annualMaintenance2;
+
+    console.log(carTotal1);
+    console.log(carTotal1 === carTotal2);
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          purchase price:
-          <input
-            type="text"
-            placeHolder="purchase price"
-            value={this.state.purchasePrice1}
-            onChange={this.handleChange}
-            name="purchasePrice1"
-          ></input>
-        </label>
-        <br></br>
-        <label>
-          miles:
-          <input
-            placeHolder="miles"
-            type="text"
-            placeHolder="purchase price"
-            value={this.state.miles1}
-            onChange={this.handleChange}
-            name="miles1"
-          ></input>
-        </label>
-        <br></br>
-        <label>
-          estimated insurance
-          <input
-            placeHolder="estimated insurance"
-            type="text"
-            placeHolder="purchase price"
-            value={this.state.estimatedInsurance1}
-            onChange={this.handleChange}
-            name="estimatedInsurance1"
-          ></input>
-        </label>
-        <br></br>
-        <label>
-          sales tax
-          <input
-            placeHolder="sales tax"
-            type="text"
-            placeHolder="purchase price"
-            value={this.state.salesTax1}
-            onChange={this.handleChange}
-            name="salesTax1"
-          ></input>
-        </label>
-        <br></br>
-        <label>
-          annual maintenance
-          <input
-            placeHolder="annual maintenance"
-            type="text"
-            placeHolder="purchase price"
-            value={this.state.annualMaintenance1}
-            onChange={this.handleChange}
-            name="annualMaintenance1"
-          ></input>
-        </label>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <h2>Car 2</h2>
-        <label>
-          purchase price:
-          <input
-            type="text"
-            placeHolder="purchase price"
-            value={this.state.purchasePrice2}
-            onChange={this.handleChange}
-            name="purchasePrice2"
-          ></input>
-        </label>
-        <br></br>
-        <label>
-          miles:
-          <input
-            placeHolder="miles"
-            type="text"
-            placeHolder="purchase price"
-            value={this.state.miles2}
-            onChange={this.handleChange}
-            name="miles2"
-          ></input>
-        </label>
-        <br></br>
-        <label>
-          estimated insurance
-          <input
-            placeHolder="estimated insurance"
-            type="text"
-            placeHolder="purchase price"
-            value={this.state.estimatedInsurance2}
-            onChange={this.handleChange}
-            name="estimatedInsurance2"
-          ></input>
-        </label>
-        <br></br>
-        <label>
-          sales tax
-          <input
-            placeHolder="sales tax"
-            type="text"
-            placeHolder="purchase price"
-            value={this.state.salesTax2}
-            onChange={this.handleChange}
-            name="salesTax2"
-          ></input>
-        </label>
-        <br></br>
-        <label>
-          annual maintenance
-          <input
-            placeHolder="annual maintenance"
-            type="text"
-            placeHolder="purchase price"
-            value={this.state.annualMaintenance2}
-            onChange={this.handleChange}
-            name="annualMaintenance2"
-          ></input>
-        </label>
-
-        <input type="submit" value="Submit" />
-      </form>
+      <div class={style.CarVsCar}>
+        <form onSubmit={this.handleSubmit}>
+        <h2>Car 1</h2>
+          <label>
+            purchase price:
+            <input
+              type="number"
+              placeHolder="purchase price"
+              value={this.state.purchasePrice1}
+              onChange={this.handleChange}
+              name="purchasePrice1"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            miles:
+            <input
+              placeHolder="miles"
+              type="number"
+              placeHolder="100,000"
+              value={this.state.miles1}
+              onChange={this.handleChange}
+              name="miles1"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            estimated insurance
+            <input
+              placeHolder="estimated insurance"
+              type="number"
+              placeHolder="$150 / month "
+              value={this.state.estimatedInsurance1}
+              onChange={this.handleChange}
+              name="estimatedInsurance1"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            sales tax
+            <input
+              placeHolder="sales tax"
+              type="number"
+              placeHolder="2.5%"
+              value={this.state.salesTax1}
+              onChange={this.handleChange}
+              name="salesTax1"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            annual maintenance
+            <input
+              placeHolder="annual maintenance"
+              type="number"
+              placeHolder="$1500"
+              value={this.state.annualMaintenance1}
+              onChange={this.handleChange}
+              name="annualMaintenance1"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            how many years do you plan to own the car?
+            <input
+              placeHolder="ownershipYears"
+              type="number"
+              placeHolder="5"
+              value={this.state.ownershipYears}
+              onChange={this.handleChange}
+              name="ownershipYears"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <h2>Car 2</h2>
+          <label>
+            purchase price:
+            <input
+              type="number"
+              placeHolder="purchase price"
+              value={this.state.purchasePrice2}
+              onChange={this.handleChange}
+              name="purchasePrice2"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            miles:
+            <input
+              placeHolder="miles"
+              type="number"
+              placeHolder="purchase price"
+              value={this.state.miles2}
+              onChange={this.handleChange}
+              name="miles2"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            estimated insurance
+            <input
+              placeHolder="estimated insurance"
+              type="number"
+              placeHolder="purchase price"
+              value={this.state.estimatedInsurance2}
+              onChange={this.handleChange}
+              name="estimatedInsurance2"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            sales tax
+            <input
+              placeHolder="sales tax"
+              type="number"
+              placeHolder="purchase price"
+              value={this.state.salesTax2}
+              onChange={this.handleChange}
+              name="salesTax2"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            annual maintenance
+            <input
+              placeHolder="annual maintenance"
+              type="number"
+              placeHolder="purchase price"
+              value={this.state.annualMaintenance2}
+              onChange={this.handleChange}
+              name="annualMaintenance2"
+              required
+            ></input>
+          </label>
+          <br></br>
+          <label>
+            how many years do you plan to own the car?
+            <input
+              placeHolder="ownershipYears2"
+              type="number"
+              placeHolder="5"
+              value={this.state.ownershipYears}
+              onChange={this.handleChange}
+              name="ownershipYears2"
+              required
+            ></input>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
